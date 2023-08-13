3217920e91ae4bc8d69a9eb0b91eb417fde6f271
Fabbediklabbe <97906894+Fabbediklabbe@users.noreply.github.com>
2023-08-13 11:15:26 +0200

    Update gambl.py
    The player can now loose the game & can no longer bet with money they don’t have

--- gambl.py
+++ gambl.py
@@ -9,6 +9,8 @@ def menu(balance):
     print ("WELCOME TO GAMBL!")
     print("MENU")
     print("(1) COINFLIP")
+    print("")
+    print("If you want to go back at any time, type 'x' when presented with an input")
     menChoice = input()
 
     if menChoice == "1":
@@ -27,18 +29,30 @@ def coinflip(balance):
 
     coin = random.randint(0,1)
 
+    
     coinflipBet = input("Bet amount: ")
-    coinflipBet = int(coinflipBet)
-    # converterar användarens bet till ett nummer 
-    # annars sparas exempelvis bettet "20" som en string, vilket vi inte kan subtrahera eller addera sen när användaren vinner eller förlorar
+    if coinflipBet == "x":
+        menu(balance)
+    else:
+        coinflipBet = int(coinflipBet)
+        # converts the users bet input to and integer
+
+    if coinflipBet > balance:
+        print("You are trying to bet money that you dont have, maybe take a loan? ;)")
+        input("(ENTER to continue)")
+        coinflip(balance)
+
+
 
     print()
     
     choise = input("Heads or tails? (h/t): ")
     if choise == "h":
-        user = "Heads"
+        guess = "Heads"
     elif choise == "t":
-        user = "Tails"
+        guess = "Tails"
+    elif choise == "x":
+        menu(balance)
     else:
         print ("Not a valid input!")
         coinflip(balance)
@@ -52,7 +66,7 @@ def coinflip(balance):
     print("The coin lands on", side)
     print("")
 
-    if user == side:
+    if guess == side:
         print("You won!")
         balance += coinflipBet
     else:
@@ -61,7 +75,19 @@ def coinflip(balance):
     
     input("(ENTER to continue)")
 
-    coinflip(balance)
+    if balance == 0:
+        bankrupt(balance)
+    else:
+        coinflip(balance)
 
+    
+
+def bankrupt(balance):
+    os.system("clear")
+    print("GAME OVER")
+    print("You have lost all of your money")
+    print()
+    input("ENTER to exit game")
+    exit()
 
 menu(balance)
